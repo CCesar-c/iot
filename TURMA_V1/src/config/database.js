ee14a77cbaf260a2b3950857b3d9e220fb122230
CESAR JOSE COLMENAREZ <cesar_colmenarez@estudante.sesisenai.org.br>
2026-08-04 15:25:01 -0300

    update

---
+++ TURMA_V1/src/config/database.js
@@ -0,0 +1,24 @@
+module.exports = {
+  username: "postgres",
+  password: "senai",
+  database: "desi_v1",
+  host: "127.0.0.1",
+  dialect: "postgres",
+  port: 5433,
+};
+
+// },
+// "test": {
+//   "username": "root",
+//   "password": null,
+//   "database": "database_test",
+//   "host": "127.0.0.1",
+//   "dialect": "mysql"
+// },
+// "production": {
+//   "username": "root",
+//   "password": null,
+//   "database": "database_production",
+//   "host": "127.0.0.1",
+//   "dialect": "mysql"
+// }
