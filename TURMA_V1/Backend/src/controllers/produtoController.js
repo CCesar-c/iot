74017a287895af0155f070c3e0cd4de63eb60c8a
CCesar-c <cesarinfluencer14@gmail.com>
2026-08-24 16:56:35 -0300

    fixed

--- TURMA_V1/Backend/src/controllers/produtoController.js
+++ TURMA_V1/Backend/src/controllers/produtoController.js
@@ -8,46 +8,42 @@ class produtoController {
     return res.status(200).json(produtos);
   }
   async store(req, res) {
-    const { nome, preco, quantidade } = req.body
+    const { nome, preco, quantidade } = req.body;
 
     const createdProduto = await produto.create({
       nome,
       preco,
       quantidade,
-    })
+    });
 
-    return res.status(200).json(createdProduto)
-
+    return res.status(200).json(createdProduto);
   }
   async update(req, res) {
-    let { id} = req.params;
-    const { nome, preco, quantidade } = req.body
+    let { id } = req.params;
+    const { nome, preco, quantidade } = req.body;
     await produto.update(
       {
         nome,
         preco,
-        quantidade
+        quantidade,
       },
       {
         where: {
-          id
-        }
-      }
-    )
-    return res.status(200).json({ message:"Produto atualizado com sucesso"})
+          id,
+        },
+      },
+    );
+    return res.status(200).json({ message: "Produto atualizado com sucesso" });
   }
   async deletar(req, res) {
-    let { id} = req.params;
-    await produto.destroy(
-      {
-        where: {
-          id
-        }
-      }
-    )
-    return res.status(200).json({ message:"Produto Deletado com sucesso"})
+    let { id } = req.params;
+    await produto.destroy({
+      where: {
+        id,
+      },
+    });
+    return res.status(200).json({ message: "Produto Deletado com sucesso" });
   }
-
 }
 
 module.exports = new produtoController();
